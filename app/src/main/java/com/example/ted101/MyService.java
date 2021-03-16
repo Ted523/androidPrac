193c98c4f55f430852abedf8b88ff49e6f346d4d
Teddy Otieno <tedotieno523@gmail.com>
2021-03-16 09:48:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ted101/MyService.java
@@ -0,0 +1,30 @@
+package com.example.ted101;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent){
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        // Let it continue running until it is stopped
+        Toast.makeText(this, "Service started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
